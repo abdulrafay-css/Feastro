c92b986da4f5df8dc63b7c242a1194f4fca4d6e4
Abdul Rafay Siddique <abdulrafay1css@gmail.com>
2026-02-12 23:39:53 +0500

    FEASTRO

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div style={{
+      backgroundColor: "#121212",
+      minHeight: "100vh",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center"
+    }}>
+      <h1 style={{ color: "#FF7A00", fontSize: "40px" }}>
+        Feastro Frontend Running 🚀
+      </h1>
+    </div>
+  )
+}
+
+export default App
